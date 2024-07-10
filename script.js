8847ff9e1a1d47d646f90d1eeee8e6a010115e12
Oteng "Pappy" Kojo Tano <otengtano123@gmail.com>
2024-07-10 11:41:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', (event) => {
     const repoOwner = 'nasbox';
     const repoName = 'nasbox.github.io';
@@ -5,7 +6,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     const fileListElement = document.getElementById('fileList');
 
-    function fetchAndDisplayContents(path = '') {
+    function fetchAndDisplayContents(path = '', parentElement = fileListElement) {
         fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${path}?ref=${branchName}`)
             .then(response => {
                 if (!response.ok) {
@@ -17,16 +18,30 @@ document.addEventListener('DOMContentLoaded', (event) => {
                 data.forEach(item => {
                     const listItem = document.createElement('li');
                     const link = document.createElement('a');
-                    link.href = item.html_url;
                     link.textContent = item.name;
                     listItem.appendChild(link);
-                    fileListElement.appendChild(listItem);
 
                     if (item.type === 'dir') {
-                        const nestedList = document.createElement('ul');
-                        listItem.appendChild(nestedList);
-                        fetchAndDisplayContents(item.path);
+                        listItem.classList.add('folder');
+                        link.href = '#';
+                        link.addEventListener('click', (e) => {
+                            e.preventDefault();
+                            const nestedList = listItem.querySelector('.nested');
+                            if (nestedList) {
+                                nestedList.style.display = nestedList.style.display === 'none' ? 'block' : 'none';
+                            } else {
+                                const newNestedList = document.createElement('ul');
+                                newNestedList.classList.add('nested');
+                                listItem.appendChild(newNestedList);
+                                fetchAndDisplayContents(item.path, newNestedList);
+                            }
+                        });
+                    } else {
+                        listItem.classList.add('file');
+                        link.href = item.html_url;
                     }
+
+                    parentElement.appendChild(listItem);
                 });
             })
             .catch(error => {
